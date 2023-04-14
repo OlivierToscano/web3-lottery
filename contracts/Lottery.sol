53735a2f06ac5add2eb86ed69481cf11afedaba6
Olivier Toscano <olivier@toscano.lu>
2023-04-14 10:45:44 +0200

    add lottery factory to be able to deploy multiple lotteries

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,16 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+contract LotteryFactory {
+    address payable[] public deployedLotteries;
+
+    function createLottery(uint minimum) public {
+        address newLottery = address(new Lottery(minimum, msg.sender));
+        deployedLotteries.push(payable(newLottery));
+    }
+
+    function getDeployedLotteries() public view returns (address payable[] memory) {
+        return deployedLotteries;
+    }
+}
+
 contract Lottery {
     address public manager;
+    uint public minimumParticipation;
     address payable[] public players;
     
-    constructor() {
-        manager = msg.sender;
+    // Emitted when the winner has been picked
+    event WinnerHasBeenPicked(address winnerAddress);
+
+    constructor(uint minimum, address creator) {
+        manager = creator;
+        minimumParticipation = minimum;
     }
     
     function enter() public payable {
-        require(msg.value > .01 ether);
+        require(msg.value > minimumParticipation); // in wei
         players.push(payable(msg.sender));
     }
     
@@ -19,8 +37,16 @@ contract Lottery {
     }
     
     function pickWinner() public restricted {
+        // pick up a random indox from players array
         uint index = random() % players.length;
+
+        // send funds to the winner
         players[index].transfer(address(this).balance);
+        
+        // return winner address value
+        emit WinnerHasBeenPicked(players[index]);
+
+        // reset players array
         players = new address payable[](0);
     }
     

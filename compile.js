735b45fc0e05344ce0cc489e6e708032f67ac05b
Olivier Toscano <olivier@toscano.lu>
2023-05-31 10:18:38 +0200

    code beautifier

--- compile.js
+++ compile.js
@@ -1,36 +1,36 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
 
-const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
-const source = fs.readFileSync(lotteryPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
 
 const input = {
-  language: 'Solidity',
-  sources: {
-    'Lottery.sol': {
-      content: source,
+    language: "Solidity",
+    sources: {
+        "Lottery.sol": {
+            content: source,
+        },
     },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*'],
-      },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
     },
-  },
 };
 
 // compile
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
 // write contract into client contracts folder
-for (var contractName in output.contracts['Lottery.sol']) {
-  fs.writeFileSync(
-    `./client/ethereum/contracts/${contractName}.json`,
-    JSON.stringify(output.contracts['Lottery.sol'][contractName])
+for (var contractName in output.contracts["Lottery.sol"]) {
+    fs.writeFileSync(
+        `./client/ethereum/contracts/${contractName}.json`,
+        JSON.stringify(output.contracts["Lottery.sol"][contractName])
     );
 }
 
 // export
-module.exports = output.contracts['Lottery.sol'].LotteryFactory;
+module.exports = output.contracts["Lottery.sol"].LotteryFactory;
